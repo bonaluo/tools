b280adfe6d8c4dd0153cbb74342f916359711ead
xiafangyuan <eeenihao8@163.com>
2024-04-04 00:11:18 +0800

    修复多余空格问题

--- scripts/windows/replace_path.bat
+++ scripts/windows/replace_path.bat
@@ -23,7 +23,7 @@ for /f "tokens=1,* delims==" %%a in (info.ini) do (
         @REM 原文件的路径分隔符是/而不是\
         set newPath=!newPath:\=/!
         echo newPath=!newPath!
-        echo %%a=!newPath! >> info.ini.new
+        echo %%a=!newPath!>> info.ini.new
     ) else (
         @REM %%a不能直接操作，需要先赋值给变量
         set "a=%%a"
@@ -36,16 +36,19 @@ for /f "tokens=1,* delims==" %%a in (info.ini) do (
                 echo set firstLine=0
                 set firstLine=0
             )
-            echo %%a >> info.ini.new
+            echo %%a>> info.ini.new
         ) else (
-            echo %%a=%%b >> info.ini.new
+            echo %%a=%%b>> info.ini.new
         )
     )
 )
 
 @REM 周三 2024/04/03 18:28:03.68
-@REM 备份原文件，并将新文件重命名为 yyyyMMddMMhhss 结尾的格式
+@REM 备份原文件为 yyyyMMddMMhhss 结尾的格式，再用新文件覆盖
 move info.ini info.ini.bak.!datetime:~3,4!!datetime:~8,2!!datetime:~11,2!!datetime:~14,2!!datetime:~17,2!!datetime:~20,2!
 move info.ini.new info.ini
+@REM 复制配置文件覆盖应用安装目录中的配置文件
+copy info.ini info.ini.copy
+move info.ini.copy "D:\Program Files\Microvirt\MEmu\videos\info.ini"
 
 pause
