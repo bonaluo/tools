916473f28e53179b5f118a01bbfbb96759992933
xiafangyuan <eeenihao8@163.com>
2024-04-05 12:47:04 +0800

    增加对睡眠电源计划存在的判断

--- scripts/windows/switch powercfg.bat
+++ scripts/windows/switch powercfg.bat
@@ -2,14 +2,34 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-@REM 手动复制一个睡眠计划
+@REM 手动复制一个电源计划，并改名为睡眠，调整睡眠计划
 @REM powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 @REM powercfg /changename 8c516f6d-1f64-44b9-b56b-d02779604e78 睡眠
-@REM 调整睡眠电源计划
 
 set "balanceGuid=381b4222-f694-41f0-9685-ff5bb260df2e"
 set "highguid=8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
 set "sleepguid=8c516f6d-1f64-44b9-b56b-d02779604e78"
+
+set findsleep=0
+for /f "skip=3 delims=" %%i in ('powercfg /l') do (
+    @REM echo ------------------------------------------
+    @REM echo %%i
+    set schemestr=%%i
+    set schemename=!schemestr:~50,2!
+    @REM echo !schemename!
+    if "!schemename!"=="睡眠" (
+        set findsleep=1
+        set sleepguid=!schemestr:~11,36!
+        echo sleepguid=!sleepguid!
+    )
+)
+
+if !findsleep!==0 (
+    echo 未找到睡眠模式
+    pause
+    exit 0
+)
+
 
 @REM 获取当前活动的睡眠计划
 
