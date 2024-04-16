27518319e032a71d2016e211cfe3875d74ab9542
fangyuan.xia <fangyuan.xia@99bill.com>
2024-04-16 11:16:25 +0800

    ivy2maven scala脚本和onedrive重新安装脚本

---
+++ scripts/scala/IvyToMavenConverter.scala
@@ -0,0 +1,56 @@
+
+import org.xml.sax.helpers.DefaultHandler
+import org.xml.sax.{Attributes, InputSource, SAXException}
+
+import java.io.StringReader
+import javax.xml.parsers.SAXParserFactory
+import scala.io.Source
+
+
+object IvyToMavenConverterWithRoot {
+
+  private def convertIvyToMaven(org: String, name: String, rev: String): String = {
+    s"""<dependency>
+       |    <groupId>$org</groupId>
+       |    <artifactId>$name</artifactId>
+       |    <version>$rev</version>
+       |</dependency>\n""".stripMargin
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(args.mkString("(", ";", ")"))
+    val resultBuilder = new StringBuilder
+    val bufferedSource = Source.fromFile(args(0))
+    val str = bufferedSource.getLines().filter(_.nonEmpty).reduce(_ + _)
+    bufferedSource.close()
+
+    // 必须要有根元素
+    val xmlData = str
+
+    val factory = SAXParserFactory.newInstance
+    val saxParser = factory.newSAXParser
+
+    val handler = new DefaultHandler() {
+      var isDependency = false
+      var org = ""
+      var name = ""
+      var rev = ""
+
+      @throws[SAXException]
+      override def startElement(uri: String, localName: String, qName: String, attributes: Attributes): Unit = {
+        if (qName.equalsIgnoreCase("dependency")) {
+          isDependency = true
+          org = attributes.getValue("org")
+          name = attributes.getValue("name")
+          rev = attributes.getValue("rev")
+          resultBuilder.append(convertIvyToMaven(org, name, rev))
+        }
+      }
+    }
+
+    saxParser.parse(new InputSource(new StringReader(xmlData)), handler)
+
+    println(resultBuilder)
+  }
+
+}
---
+++ scripts/windows/onedrive_reinstall.bat
@@ -0,0 +1,3 @@
+@echo off
+winget uninstall Microsoft.OneDrive && winget install Microsoft.OneDrive
+pause
