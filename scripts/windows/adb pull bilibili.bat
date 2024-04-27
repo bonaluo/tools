36379b10d3671fb460f64a42004dc559f646c672
fangyuan.xia <eeenihao8@163.com>
2024-04-28 04:11:08 +0800

    增加耗时打印

--- scripts/windows/adb pull bilibili.bat
+++ scripts/windows/adb pull bilibili.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 
 :: 设置日期和时间格式
 for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set "dt=%%a"
+set "starttime=!dt:~0,4!-!dt:~4,2!-!dt:~6,2! !dt:~8,2!:!dt:~10,2!:!dt:~12,2!"
 set "year=!dt:~0,4!"
 set "month=!dt:~4,2!"
 set "day=!dt:~6,2!"
@@ -16,6 +17,7 @@ set "targetDir=F:\video\bilibili\bilibili_download_!year!!month!!day!!hour!!minu
 mkdir "!targetDir!"
 
 :: 使用ADB拉取文件
+:: temp.txt 是utf-8编码，batch读入之后乱码
 adb shell "cd /sdcard/Android/data/tv.danmaku.bili/download/ && ls" > temp.txt
 for /f %%f in (temp.txt) do (
     adb pull "/sdcard/Android/data/tv.danmaku.bili/download/%%f" "!targetDir!\%%f"
@@ -25,5 +27,10 @@ for /f %%f in (temp.txt) do (
 del temp.txt
 
 echo 文件已成功拉取到目录：!targetDir!
+
+:: 打印拉取耗时
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set "endtime=%%a"
+set "endtime=!endtime:~0,4!-!endtime:~4,2!-!endtime:~6,2! !endtime:~8,2!:!endtime:~10,2!:!endtime:~12,2!"
+echo !starttime!~!endtime!
 
 pause
