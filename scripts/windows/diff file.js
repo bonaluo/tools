0f79e03b472e094facec73731d8d57cc823cfbf0
fangyuan.xia <eeenihao8@163.com>
2024-05-16 02:03:05 +0800

    文件数量对比

---
+++ scripts/windows/diff file.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+try {
+    // 读取文件内容
+    const data = fs.readFileSync('E:\\note\\tools\\temp\\temp.txt', 'utf-8');
+    // 按换行符拆分内容
+    const lines = data.split(/\r?\n/);
+    // 输出每一行
+    for (const line of lines) {
+        if (!fs.existsSync("F:\\video\\telegram\\20240515\\" + line)) {
+            console.log(line);
+        }
+    }
+} catch (error) {
+    console.error('读取文件出错：', error.message);
+}
